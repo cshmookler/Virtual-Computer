3411356b22f0fcd7985b61877a035002402cf639
cshmookler <94317938+cshmookler@users.noreply.github.com>
2021-11-14 18:34:29 -0700

    Added tools for compiling

---
+++ data/read.bat
@@ -0,0 +1,3 @@
+cd C:\Users\human\Desktop\OpenGL\Virtual_Computer\data
+binary_read_and_write.exe r bin_data\rom.dat read_and_write_IO.txt
+cmd /k
---
+++ data/write.bat
@@ -0,0 +1,3 @@
+cd C:\Users\human\Desktop\OpenGL\Virtual_Computer\data
+binary_read_and_write.exe w read_and_write_IO.txt bin_data\rom.dat
+cmd /k
