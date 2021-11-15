a91740645b215f8c4b93c3e1cafdae414d2ba5e4
cshmookler <94317938+cshmookler@users.noreply.github.com>
2021-11-15 00:24:19 -0700

    Removed unnecessary linker references

--- data/compile (no console).bat
+++ data/compile (no console).bat
@@ -1,2 +1,2 @@
-g++ binary_read_and_write_source.cpp -mwindows -lmingw32 -lopengl32 -lglu32 -lfreeglut -o binary_read_and_write.exe
+g++ binary_read_and_write_source.cpp -mwindows -lmingw32 -o binary_read_and_write.exe
 cmd /k
---
+++ programs/assembler/compile (no console).bat
@@ -0,0 +1,2 @@
+g++ assembler_source_(pseudo_code).cpp -mwindows -lmingw32 -o assembler_source_(pseudo_code).exe
+cmd /k
